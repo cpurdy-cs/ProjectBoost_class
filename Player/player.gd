558c8aab0d1c1ab1d535963da4ae11f7fc9beeec
Chad Purdy <cpurdy@flourbluffschools.org>
2026-01-12 12:31:04 -0600

    add moving obstacle

--- Player/player.gd
+++ Player/player.gd
@@ -4,6 +4,8 @@ class_name Player
 @export_range(750,2500) var thrust := 1000.0
 @export var torque_thrust := 100.0
 
+var transitioning := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,33 +13,36 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * thrust)
-		
-	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0.0, 0.0, delta*torque_thrust))
-		
-	if Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0.0, 0.0, -delta*torque_thrust))
+	if not transitioning:
+		if Input.is_action_pressed("boost"):
+			apply_central_force(basis.y * delta * thrust)
+			
+		if Input.is_action_pressed("rotate_left"):
+			apply_torque(Vector3(0.0, 0.0, delta*torque_thrust))
+			
+		if Input.is_action_pressed("rotate_right"):
+			apply_torque(Vector3(0.0, 0.0, -delta*torque_thrust))
 
-	# rotate right
 
 func crash_sequence() -> void:
+	transitioning = true
 	print("Kaboom!")
-	await get_tree().create_timer(2.5).timeout
+	await get_tree().create_timer(1).timeout
 	get_tree().reload_current_scene.call_deferred()
 	
 func level_complete(next_level_file) -> void:
-	await get_tree().create_timer(2.5).timeout
+	transitioning = true
+	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file.call_deferred(next_level_file)
 	
 func _on_body_entered(body: Node) -> void:
-	if "Goal" in body.get_groups():
-		print('You win!')
-		if body.file_path:
-			level_complete(body.file_path)
-		else:
-			print("No next level found!")
+	if not transitioning:
+		if "Goal" in body.get_groups():
+			print('You win!')
+			if body.file_path:
+				level_complete(body.file_path)
+			else:
+				print("No next level found!")
 
-	if "Hazard" in body.get_groups():
-		crash_sequence()
+		if "Hazard" in body.get_groups():
+			crash_sequence()
